61699312e3f35973458fdfb0f61b22c81ae78c1c
Brett <54408245+kowh-ai@users.noreply.github.com>
2020-09-08 09:31:40 +0200

    Update travis

--- bin/travis-run.bash
+++ bin/travis-run.bash
@@ -1,11 +1,4 @@
-#!/bin/sh -e
-set -ex
+#!/bin/bash
+set -e
 
-flake8 --version
-# stop the build if there are Python syntax errors or undefined names
-flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan,ckanext-xloader
-
-pytest --ckan-ini subdir/test.ini --cov=ckanext.xloader ckanext/xloader/tests
-
-# strict linting
-flake8 . --count --max-complexity=27 --max-line-length=127 --statistics --exclude ckan,ckanext-xloader
+pytest --ckan-ini=subdir/test.ini --cov=ckanext.xloader --disable-warnings ckanext/xloader/tests
